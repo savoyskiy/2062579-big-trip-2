5997ff26d75aa32b4779f205ca871c3010c26130
savoyskiy <78842859+savoyskiy@users.noreply.github.com>
2026-06-24 15:19:16 +0300

    Исправляет работу html-webpack-plugin - добавляет импорт плагина в webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
