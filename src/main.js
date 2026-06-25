28ca702d63161b1d5acd3fee75e163b1c7546788
savoyskiy <78842859+savoyskiy@users.noreply.github.com>
2026-06-25 17:38:00 +0300

    Добавляет на страницу блок Сортировка и добавляет файлы view для точек маршрута, создания и редактирования

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import TripInfoView from './view/trip-info-view.js';
 import FilterView from './view/filter-view.js';
+import SortingView from './view/sorting-view.js';
 import { RenderPosition, render } from './render.js';
 
 const tripInfoContainer = document.querySelector('.trip-main');
 const filterContainer = document.querySelector('.trip-controls__filters');
+const tripEventsContainer = document.querySelector('.trip-events');
 
 render(new TripInfoView(), tripInfoContainer, RenderPosition.AFTERBEGIN);
 render(new FilterView(), filterContainer);
+render(new SortingView(), tripEventsContainer);
