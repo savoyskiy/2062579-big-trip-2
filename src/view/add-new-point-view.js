a6861e130aca6495fe68cbe28f91b6f616768773
savoyskiy <78842859+savoyskiy@users.noreply.github.com>
2026-07-02 12:34:33 +0300

    Проверяет отображение \n в ПР

--- src/view/add-new-point-view.js
+++ src/view/add-new-point-view.js
@@ -1,6 +1,7 @@
 import { createElement } from '../render.js';
 
-const createAddNewPointTemplate = () => `<li class="trip-events__item">
+const createAddNewPointTemplate = () =>
+  `<li class="trip-events__item">
     <form class="event event--edit" action="#" method="post">
       <header class="event__header">
         <div class="event__type-wrapper">
